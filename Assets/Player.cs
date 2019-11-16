e0f55a9530ade35cb5c38af2ce06bcbceb35b0a1
Anders <anders@daylon.se>
2019-11-16 11:59:48 +0100

    new stettings

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
 
     private Rigidbody2D rb2D;
-    private float thrust = 20.0f;
+    private float thrust = 22.0f;
     private Vector3 vectorUp;
     private bool midAir = false;
     private float horizontalLevel;
@@ -25,26 +25,22 @@ public class Player : MonoBehaviour
     {
         print("Spawning Player");
         distToGround = GetComponent<SpriteRenderer>().bounds.extents.y;
+        rb2D.freezeRotation = true;
     }
 
     public bool isGrounded()
     {
-        distToGround = GetComponent<SpriteRenderer>().bounds.extents.y;
-        //7        print(transform.position);
-        //        print(distToGround);
-        //        print(Physics2D.Raycast(transform.position, -Vector2.up, Mathf.Infinity));
+        //Objects which are considered ground must be labled "Ground" in the UI
         LayerMask mask = LayerMask.GetMask("Ground");
-        RaycastHit2D r = Physics2D.Raycast(transform.position, -Vector2.up, distToGround+ 0.1f, mask);
+        RaycastHit2D r = Physics2D.Raycast(transform.position, -Vector2.up, distToGround, mask);
 
         return (r.collider != null);
     }
     // Update is called once per frame
     void Update()
     {
-        if(isGrounded() )
+        if(isGrounded())
         {
-
-
             if (Input.GetKeyDown(KeyCode.Space))
             {
 
@@ -67,7 +63,7 @@ public class Player : MonoBehaviour
                 //rb2D.AddForce(vectorLeft * 10);
                 rb2D.velocity = new Vector3(-2, 0, 0);
             }
-        }
+        } //Else case for stuff in the air. E.g flying
 
 
 
